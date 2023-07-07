bc049422ba19d3df68b90bfe8f4766f246c9d6fa
ranasinghprateek123 <7752955637pra@gmail.com>
2023-07-07 11:21:00 +0530

    xyz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import SearchForm from './components/SearchForm'
+import Datadisplay from './components/Datadisplay'
+import RepoForm from './components/RepoForm'
+import Pagination from './Pagination'
+import "./App.css"
 
-function App() {
+export default function App() {
+  const [userData,setUserData] = useState(null)
+  const [repos,setRepos] = useState([])
+  const [currentPage,setCurrentPage] = useState(1)
+  const [perPage] =useState(5)
+  const [page,setPage] = useState(1)
+  const fetchData = async(username)=>{
+    try{
+       const userResponse =await fetch(`https://api.github.com/users/${username}`)
+       if(userResponse.ok){
+        const userData = await userResponse.json()
+        setUserData(userData)
+        console.log(userData)
+
+
+        const repoResponse = await fetch(`${userData.repos_url}?page=${page}`)
+        if(repoResponse.ok){
+          const reposData = await repoResponse.json()
+          setRepos(reposData)
+          console.log(reposData)
+        }else {
+          throw new Error("Failed to fetch any user repository")
+        }
+       } else {
+        throw new Error("User Not Found")
+       }
+      } catch(error){
+        console.log(error)
+        setUserData(null)
+      }
+  }
+const indexOfLastRepo = currentPage * perPage
+  const indexOfFirstRepo =  indexOfLastRepo - perPage
+  const currentRepos = repos.slice(indexOfFirstRepo,indexOfLastRepo)
+  const paginate =(pageNumber) =>{
+    setCurrentPage(pageNumber)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>GitHub user Finder</h1>
+      <SearchForm fetchData = {fetchData}/>
+      {userData && <Datadisplay user={userData}/>}
+      {currentRepos.length>0?(
+        <>       
+        <RepoForm repos={currentRepos} page={page} setPage={setPage}/>
+        <Pagination 
+        totalRepos={repos.length}
+        reposPerPage={perPage}
+        currentPage={currentPage}
+        paginate={paginate}/>
+        </>
+      ):(
+        <div>
+          <p>No Repository Found</p>
+        </div>
+      )}
+      
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/RepoForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function RepoForm({repos}) {
+  return (
+    <div className='repo-list'>
+        <h2>Repositories</h2>
+        <ul>
+            {repos.map((repo)=> (
+                <li key={repo._id}>
+                    <a href={repo.html_url} target='_blank' rel='noreferrer'>{repo.name}</a>
+                </li>
+            ))}
+        </ul>
+        
+    </div>
+  )
+}
